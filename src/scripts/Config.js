4142da4d4a1e0a03f8e16c873e71bf2cc6757f47
coselove <coselove@yandex.ru>
2020-12-27 12:46:35 +0300

    Проектная работа 8 v.2.4

--- src/scripts/Config.js
+++ src/scripts/Config.js
@@ -1,7 +1,7 @@
 export const params = {
     formSelector: ".popup__container",
     formSelectorPoup: ".popup__container",
-    formSelectorCard: ".popup-card__container",
+    formSelectorCard: ".popup__container_card",
     inputSelector: ".popup__input-text",
     submitButtonSelector: ".popup__save-button",
     inactiveButtonClass: "popup__save-button_inactive",
